7d561cc4f0ee0de92d0c33fe6926a96a71a5c78f
zeeshanrafique <zeeshantariqrafique@gmail.com>
2020-03-13 21:39:55 +0530

    PyFiTransfer 1 Added transfer data function

--- socketClient.py
+++ socketClient.py
@@ -6,6 +6,7 @@
 
 import socket
 class SocketClient:
+    connected = False
     def __init__(self):
         try:
             self.socketClient = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
@@ -16,7 +17,17 @@ class SocketClient:
     def connect(self,host,port):
         try:
             self.socketClient.connect((host,port))
+            self.connected = True
             print ("Connected to {} through port {} ".format(host,port))
         except socket.error as err:
             print("Error while connecting to host {} and port {} with : {}".format(host,port,err))
+
+    def transfer(self,data):
+        if not self.connected:
+            print("Not Conncted to any host")
+        else:
+            try:
+                self.socketClient.send(data)
+            except socket.error as err:
+                print("Error while sending data")
 
