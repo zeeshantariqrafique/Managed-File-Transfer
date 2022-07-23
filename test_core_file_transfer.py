476239a328be9aa4f69a2c7e93bceb63e8993c2a
zeeshantariqrafique <zeeshantariqrafique@gmail.com>
2022-07-23 10:47:52 +0530

    Fix Tests

--- test_core_file_transfer.py
+++ test_core_file_transfer.py
@@ -20,7 +20,7 @@ class TestPythonFileTransfer(unittest.TestCase):
         test_str = 'This is a test string'
         self.test_file.write(test_str)
         compressed_file_name = pyft.compress_file(self.file_to_be_compressed)
-        decompressed_file_name = pyft.decompress_file(compressed_file_name,self.test_file)
+        decompressed_file_name = pyft.decompress_file(compressed_file_name,self.expected_compressed_file_name)
         print(f'Decompressed file name is {decompressed_file_name}')
 
     def tearDown(self) -> None:
