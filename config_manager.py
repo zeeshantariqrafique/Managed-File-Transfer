415b332fd91592c5f3ea883b46459bf63155cb68
zeeshantariqrafique <zeeshantariqrafique@gmail.com>
2023-07-08 10:37:14 +0200

    Refactor ConfigManager

--- config_manager.py
+++ config_manager.py
@@ -1,53 +1,36 @@
-
-# Author : Zeeshan Tariq Rafique
-# Example usage
-#configObj = ConfigManager()
-#config = configObj.get_config()
-#port = config.listen_port
-
-from shutil import ExecError
 import yaml
 from collections import defaultdict
 
 
 class ConfigManager:
-    
-    __shared_config = defaultdict(
-        lambda: 'MFT',
-        key='instance')
+    __shared_config = defaultdict(lambda: 'MFT')
+    __shared_instance = None
 
-    __shared_instance = 'MFT'
- 
     @staticmethod
     def get_instance():
         """Static Access Method"""
-        if ConfigManager.__shared_instance == 'MFT':
+        if not ConfigManager.__shared_instance:
             ConfigManager()
         return ConfigManager.__shared_instance
- 
+
     def __init__(self):
- 
-        """virtual private constructor"""
-        if ConfigManager.__shared_instance != 'MFT':
-            raise Exception ("This class is a ConfigManager class !")
+        """Virtual private constructor"""
+        if ConfigManager.__shared_instance:
+            raise Exception("This class is a ConfigManager class!")
         else:
             ConfigManager.__shared_instance = self
-            
+
     def get_config(self):
         return self.__shared_config
 
-    def get_config_from_file(
-        self,
-        file_path: str
-        ) -> dict:
-        '''
-        file_path : The fully qualified config file path to be read
+    def get_config_from_file(self, file_path: str) -> dict:
+        """
         Read the given file_path and return a key-value dictionary with all
-        key values from config
-        '''
+        key values from the config
+        """
         with open(file_path, encoding='utf-8') as file:
             data = yaml.full_load(file)
-        config_dict = {**data['socketserver'], **data['transfer']} 
-        config_dict = { str(k).strip() : str(v).strip() for k ,v in config_dict.items() }
+        config_dict = {**data['socketserver'], **data['transfer']}
+        config_dict = {str(k).strip(): str(v).strip() for k, v in config_dict.items()}
         self.__shared_config.update(config_dict)
         return dict(config_dict.items())
