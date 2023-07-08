a28d775b7fa496b6f40962687ee00b64cd817744
zeeshantariqrafique <zeeshantariqrafique@gmail.com>
2023-07-08 10:41:40 +0200

    Refactor PythonFileTransfer

--- core_file_transfer.py
+++ core_file_transfer.py
@@ -1,85 +1,67 @@
 import os
-import sys
 import traceback
-from inspect import currentframe , stack
-from threading import Thread
 import zlib
+from inspect import currentframe, stack
+
 
 class PythonFileTransfer:
-
-    def compress_file(
-        self,
-        file: str
-        ) -> str:
-        '''compress the file passed to the function to gz format '''
+    @staticmethod
+    def compress_file(file: str) -> str:
+        '''Compress the file passed to the function to .gz format.'''
         try:
             print(f'Trying to compress file ==> {file}')
-            with open(file,'rb') as f:
+            with open(file, 'rb') as f:
                 data = f.read()
-            compressed_data=zlib.compress(data,9)
-            compressed_file_name = str(file) + ".gz"
+            compressed_data = zlib.compress(data, 9)
+            compressed_file_name = file + ".gz"
             print(f"Compressed file name ==> {compressed_file_name}")
-            with open(compressed_file_name,'wb') as of:
+            with open(compressed_file_name, 'wb') as of:
                 of.write(compressed_data)
             print("File successfully compressed")
             return compressed_file_name
         except Exception as e:
             raise e
 
-
-    def decompress_file(
-        self,
-        src_file: str,
-        dest_file: str
-        ) -> None:
-        '''Decompress the file name passed as parameter to this function'''
+    @staticmethod
+    def decompress_file(src_file: str, dest_file: str) -> None:
+        '''Decompress the file name passed as a parameter to this function.'''
         try:
-            with open(src_file,'rb') as f:
+            with open(src_file, 'rb') as f:
                 data_in_bytes = f.read()
                 data = zlib.decompress(data_in_bytes)
-            with open(dest_file,'wb') as f:
-                f.write(bytes(data))
+            with open(dest_file, 'wb') as f:
+                f.write(data)
         except Exception as e:
             raise e
 
-    def create_file(
-        self,
-        file_name: str,
-        file_path: str,
-        file_data_in_bytes: bytes
-        ) -> bool:
-        '''This function creates the file on the path recived by the socket server'''
+    @staticmethod
+    def create_file(file_name: str, file_path: str, file_data_in_bytes: bytes) -> bool:
+        '''Create the file on the path received by the socket server.'''
         try:
-            compressed_file_name = os.path.join(file_path,file_name+'.gz')
-            original_file_name = os.path.join(file_path,file_name)
+            compressed_file_name = os.path.join(file_path, file_name + '.gz')
+            original_file_name = os.path.join(file_path, file_name)
             print(type(file_data_in_bytes))
             print(file_data_in_bytes)
-            f = open(compressed_file_name, 'wb+')
-            f.write(file_data_in_bytes)
+            with open(compressed_file_name, 'wb+') as f:
+                f.write(file_data_in_bytes)
             pyft = PythonFileTransfer()
-            pyft.decompress_file(compressed_file_name,original_file_name)
+            pyft.decompress_file(compressed_file_name, original_file_name)
             print(f'Successfully wrote to destination path {original_file_name}')
             return True
         except Exception:
             traceback.print_exc()
             return False
-        finally:
-            f.close()
 
-    def create_transfer_command(
-        self,
-        src_file_name: str,
-        dest_file_path: str,
-        file_data: str
-        ) -> bytearray:
-        '''creates a single socket command including metadata
+    @staticmethod
+    def create_transfer_command(src_file_name: str, dest_file_path: str, file_data: str) -> bytearray:
+        '''Create a single socket command including metadata
         and actual file data to be transferred to destination
-        remote host in bytes over tcp sockets'''
-
-        cmd = f'''FILE_KEY|{src_file_name}|{dest_file_path}|{file_data}'''
+        remote host in bytes over TCP sockets.'''
+        cmd = f'FILE_KEY|{src_file_name}|{dest_file_path}|{file_data}'
         return bytearray(cmd, 'utf8')
-    
-    def print_log(value : str)-> None:
+
+    @staticmethod
+    def print_log(value: str) -> None:
         function = stack()[1][3]
-        filename = os.path.splitext(os.path.basename(stack()[1].filename ))[0]
+        filename = os.path.splitext(os.path.basename(stack()[1].filename))[0]
         print(f'{filename}::{function}::{currentframe().f_back.f_lineno} => {value}')
